f9115ccffb108ed9c8b8be469d6e57de7c5abb1a
acwhitlock <amelia.c.whitlock@gmail.com>
2026-04-15 13:09:38 -0700

    css and other html files are now connected to the website

--- app.js
+++ app.js
@@ -1,30 +1,9 @@
-// const http = require("http");
-// const fs = require("fs");
-
-// const server = http.createServer((req, res) => {
-//     fs.readFile("index.html", (err, data) => {
-//         res.writeHead(200,{"Content-Type": "text/html"});
-//         res.write(data);
-//         res.end();
-//     })
-
-// });
-
-// server.listen(3000, () => {
-//     console.log("Server running on http://localhost:3000");
-// })
-
 const express = require("express");
 const app = express();
-const path = require("path")
-app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"index.html"));
+const path = require("path");
+const PORT = 3000;
+app.use(express.static(path.join(__dirname,"")));
 
+app.listen(PORT, ()=>{
+    console.log(`Server is running at http://localhost:${PORT}`)
 });
-
-// app.use(express.urlencoded({extended:false}));
-// app.post("/form",(req,res) => {
-//     res.send(req.body.name);
-// })
-
-app.listen(3000);
