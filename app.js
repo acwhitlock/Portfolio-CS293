d966e221aae9d8d3acea7052c693cf69d8dbf96d
acwhitlock <amelia.c.whitlock@gmail.com>
2026-05-06 16:10:23 -0700

    adjusted dark mode implementation - trying to use local storage to keep the dark mode when switching between pages

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3000;
-app.use(express.static(path.join(__dirname,"")));
+app.use(express.static(path.join(__dirname, "")));
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`)
 });
